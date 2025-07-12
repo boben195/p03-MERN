863ff9ca17ab1750c1ffe3c935cbfd1a794b36a8
boben195 <boben195@ukr.net>
2025-07-12 10:54:01 +0300

    add base for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-green-300 text-6x1">TESSSSSSSSSST</h1>
+    </div>
+  );
+};
+
+export default App;
